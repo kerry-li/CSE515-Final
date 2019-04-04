895cff44b3f8d63b8393e3d156d9b362f9d30346
kerry-li <kerryli1997@gmail.com>
2019-04-03 19:11:40 -0500

    filled blr

--- src/learn/blr.py
+++ src/learn/blr.py
@@ -1,1 +1,14 @@
 # Bayesian linear regression.
+
+from sklearn.linear_model import BayesianRidge
+
+def fit(X, y):
+    return BayesianRidge().fit(X, y)
+
+def main():
+    from src.data import parseData
+    X, y = parseData('../../data/winequality-red.csv')
+    fit(X, y)
+
+if __name__ == '__main__':
+    main()
