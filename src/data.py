35fa7a181aa8c59abc72303cccb0ca07f54c78b5
kerry-li <kerryli1997@gmail.com>
2019-04-03 19:25:06 -0500

    function to split itraining and val samples

--- src/data.py
+++ src/data.py
@@ -1,6 +1,7 @@
 # Contains a function which returns feature and label matrices given a
 # csv file.
 
+import numpy as np
 import pandas as pd
 
 # Returns X, y parsed from file name.
@@ -10,8 +11,18 @@ def parseData(fileName, delimiter=';'):
     y = allData.values[:, -1:]
     return X, y
 
+def splitData(n, percentTraining):
+    samples = np.arange(n)
+    np.random.shuffle(samples)
+    numberTraining = np.ceil(n * percentTraining).astype(int)
+    trainingSamples = samples[:numberTraining]
+    valSamples = samples[numberTraining:]
+    return trainingSamples, valSamples
+
 def main():
     parseData('../data/winequality-red.csv')
+    print(splitData(100, .871))
+    print(splitData(100, 1))
 
 if __name__ == '__main__':
     main()
