4b896fb82fb513fff5df0899036cf344f5857fce
golivo19 <golivo@wustl.edu>
2019-04-03 19:00:40 -0500

    added gpr

--- src/learn/gpr.py
+++ src/learn/gpr.py
@@ -1,1 +1,10 @@
 # Gaussian process regression.
+
+from sklearn.gaussian_process import GaussianProcessRegressor as gpr
+from sklearn.gaussian_process.kernels import RBF
+
+def gaussianProcess(X, y, kernel=None):
+    # If the kernel parameter is None, then 1.0 * RBF(1.0) is used.
+    gp = gpr(kernel=kernel, optimizer=None)
+    gp.fit(X,y)
+    return gp
