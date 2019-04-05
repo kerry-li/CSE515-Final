2c99c7973b3c9bec7aff231b1a39298e819280dd
kerry-li <kerryli1997@gmail.com>
2019-04-05 16:01:54 -0500

    run on white wine

--- src/main.py
+++ src/main.py
@@ -6,31 +6,34 @@ import numpy as np
 import random
 
 WHITE_WINE_FILENAME = '../data/winequality-white.csv'
+RED_WINE_FILENAME = '../data/winequality-red.csv'
 
 PERCENT_TRAINING = 0.8
+
+def trainAndValidate(fileName):
+    trainingData, valData = data.parseAndSplit(WHITE_WINE_FILENAME,
+                                               ';',
+                                               PERCENT_TRAINING)
+    trainingX, trainingY = trainingData
+    valX, valY = valData
+
+    linearFit = blr.fit(trainingX, trainingY)
+    gprFit = gpr.trainGaussianProcess(trainingX, trainingY)
+
+    linearPredict = linearFit.predict(valX)
+    gprPredict = gprFit.predict(valX)
+
+    whiteLinearMse = ((linearPredict - valY) ** 2).mean()
+    whiteGpMse = ((gprPredict - valY) ** 2).mean()
+
+    return whiteLinearMse, whiteGpMse
 
 def main():
     random.seed(0)
     np.random.seed(0)
-    whiteTrainingData, whiteValData = data.parseAndSplit(WHITE_WINE_FILENAME,
-                                                         ';',
-                                                         PERCENT_TRAINING)
-    whiteTrainingX, whiteTrainingY = whiteTrainingData
-    whiteValX, whiteValY = whiteValData
 
-    whiteLinear = blr.fit(whiteTrainingX, whiteTrainingY)
-    whiteGp = gpr.trainGaussianProcess(whiteTrainingX, whiteTrainingY)
-
-    whiteLinearPredict = whiteLinear.predict(whiteValX)
-    whiteGpPredict = whiteGp.predict(whiteValX)
-
-    whiteLinearMse = ((whiteLinearPredict - whiteValY) ** 2).mean()
-    whiteGpMse = ((whiteGpPredict - whiteValY) ** 2).mean()
-
-    print(whiteLinearMse)
-    print(whiteGpMse)
-
-
+    print(trainAndValidate(WHITE_WINE_FILENAME))
+    print(trainAndValidate(RED_WINE_FILENAME))
 
 
 if __name__ == '__main__':
