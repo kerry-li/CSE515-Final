3601bf405eef6bb52508374aafe6195a1510af7a
kerry-li <kerryli1997@gmail.com>
2019-04-05 15:25:50 -0500

    added class for automatic kernel selection

--- src/learn/gpr.py
+++ src/learn/gpr.py
@@ -3,6 +3,19 @@
 from sklearn.gaussian_process import GaussianProcessRegressor as gpr
 from sklearn.gaussian_process.kernels import RBF
 
+# Class for automated model selection described by the method at
+# https://pdfs.semanticscholar.org/cc27/639170d87581e2e1ecdc4dca3716915619d2.pdf
+# using only GPR priors on the latent function.
+class AutoKernelGpr:
+
+    def __init__(self, baseKernels):
+        self.baseKernels = baseKernels
+
+    # Returns the trained GPR with the optimal kernel found after searching
+    # for /rounds/ rounds.
+    def searchForRounds(self, rounds):
+        pass
+
 def trainGaussianProcess(X, y, kernel=None):
     # If the kernel parameter is None, then 1.0 * RBF(1.0) is used.
     gp = gpr(kernel=kernel)
